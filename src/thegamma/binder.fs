8f885e2f9fee5bff1b18b1d30647f281aa1bcb6f
Tomas Petricek <tomas@tomasp.net>
2016-09-17 03:36:33 +0100

    Add forgotten binder files

---
+++ src/thegamma/binder.fs
@@ -0,0 +1,131 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Binder attaches `Entity` objects to `Node` objects produced by the parser
+// Entities are reused when possible and contain inferred types etc.
+// ------------------------------------------------------------------------------------------------
+module TheGamma.Binder
+
+open TheGamma.Ast
+open TheGamma.Common
+
+/// As we bind, we keep root entity, current scope & variables in scope
+type BindingContext = 
+  { Variables : Map<Name, Entity>  
+    Scope : Entity
+    Root : Entity
+    /// Table with previously created entities. This is a mutable mapping from 
+    /// list of symbols (antecedent entities) together with entity kind & name
+    /// to the actual entity. Antecedents capture dependencies (if dependency 
+    /// changed, we need to recreate the entity that depends on them)
+    Table : ListDictionary<Symbol, Map<EntityKind * Name, Entity>> 
+    /// Collects all bound entities and their ranges
+    Bound : ResizeArray<Range * Entity> }
+
+
+/// Lookup entity (if it can be reused) or create & cache a new one
+let bindEntity ctx kind antecedents name =
+  let symbols = antecedents |> List.map (fun a -> a.Symbol)
+  let nestedDict = 
+    match ListDictionary.tryFind symbols ctx.Table with
+    | None -> Map.empty
+    | Some res -> res
+  if nestedDict.ContainsKey (kind, name) then 
+    Log.trace("binder", "Cached: binding %s %s", formatEntityKind kind, name.Name)
+    nestedDict.[kind, name]
+  else
+    Log.trace("binder", "New: binding %s %s", formatEntityKind kind, name.Name)
+    let symbol = createSymbol ()
+    let entity = { Kind = kind; Antecedents = antecedents; Name = name; Symbol = symbol; Type = None; Errors = [] }
+    ListDictionary.set symbols (Map.add (kind, name) entity nestedDict) ctx.Table
+    entity    
+
+/// Assign entity to a node in parse tree
+let setEntity ctx node entity = 
+  ctx.Bound.Add(node.Range, entity)
+  node.Entity <- Some entity
+  entity
+
+/// Used for entities with no name
+let anonymous = { Name.Name = "" }
+
+/// Bind entities to expressions in the parse tree
+/// (See `EntityKind` for explanation of how the entity tree looks like)
+let rec bindExpression callSite ctx node = 
+  let bindCallArgExpression site = bindExpression (Some site)
+  let bindExpression = bindExpression None
+  match node.Node with
+  | Expr.Variable(name) ->
+      match ctx.Variables.TryFind name.Node with 
+      | Some decl -> bindEntity ctx EntityKind.Variable [decl] name.Node |> setEntity ctx node
+      | None -> bindEntity ctx EntityKind.GlobalValue [ctx.Scope] name.Node |> setEntity ctx node
+
+  | Expr.Call(instExpr, name, argsNode) ->
+      // Bind instance & create call site that depends on it
+      let inst = defaultArg (Option.map (bindExpression ctx) instExpr) ctx.Root
+      let site = bindEntity ctx EntityKind.CallSite [inst] name.Node
+      // Bind arguments - which depend on the call site
+      let args = argsNode.Node |> List.map (fun arg -> 
+        let expr = bindCallArgExpression site ctx arg.Value
+        match arg.Name with 
+        | Some n -> bindEntity ctx EntityKind.NamedParam [expr] n.Node |> setEntity ctx n
+        | None -> expr)
+      let args = bindEntity ctx EntityKind.ArgumentList (ctx.Root::args) anonymous |> setEntity ctx argsNode
+      bindEntity ctx (EntityKind.ChainElement(instExpr.IsSome, false)) [inst; args] name.Node 
+        |> setEntity ctx node |> setEntity ctx name
+
+  | Expr.Property(expr, name) ->
+      let ante = bindExpression ctx expr
+      bindEntity ctx (EntityKind.ChainElement(true, true)) [ante] name.Node 
+        |> setEntity ctx node |> setEntity ctx name
+
+  | Expr.Binary(l, op, r) ->
+      let lentity = bindExpression ctx l
+      let rentity = bindExpression ctx r
+      bindEntity ctx (EntityKind.Operator op.Node) [lentity; rentity] anonymous |> setEntity ctx node
+
+  | Expr.List(els) ->
+      let entities = els |> List.map (bindExpression ctx)      
+      bindEntity ctx EntityKind.List entities anonymous |> setEntity ctx node
+
+  | Expr.Function(v, e) ->
+      let scope = bindEntity ctx EntityKind.Scope [ctx.Scope] anonymous
+      let varParent = match callSite with None -> [scope] | Some s -> [scope; s]
+      let var = bindEntity ctx EntityKind.Binding varParent v.Node |> setEntity ctx v
+      let body = bindExpression { ctx with Variables = Map.add v.Node var ctx.Variables } e
+      bindEntity ctx EntityKind.Function [var; body] anonymous |> setEntity ctx node
+
+  | Expr.Boolean b -> bindEntity ctx (EntityKind.Constant (Constant.Boolean b)) [ctx.Root] anonymous |> setEntity ctx node
+  | Expr.String s -> bindEntity ctx (EntityKind.Constant (Constant.String s)) [ctx.Root] anonymous |> setEntity ctx node
+  | Expr.Number n -> bindEntity ctx (EntityKind.Constant (Constant.Number n)) [ctx.Root] anonymous |> setEntity ctx node
+  | Expr.Empty -> bindEntity ctx (EntityKind.Constant Constant.Empty) [ctx.Root] anonymous |> setEntity ctx node
+
+/// Bind entities to a command in a parse tree. The handling of `let` is similar
+/// to the handling of lambda abstraction. This adds variables to context - we ignore
+/// bound entities, because nothing depends on it (except via variables)
+let bindCommand ctx node =
+  match node.Node with
+  | Command.Let(v, e) ->
+      let body = bindExpression None ctx e 
+      let scope = bindEntity ctx EntityKind.Scope [ctx.Scope] anonymous
+      let var = bindEntity ctx EntityKind.Variable [body] v.Node |> setEntity ctx v
+      bindEntity ctx EntityKind.Command [var; body] anonymous |> setEntity ctx node |> ignore
+      { ctx with Variables = Map.add v.Node var ctx.Variables }
+
+  | Command.Expr(e) ->
+      let body = bindExpression None ctx e 
+      bindEntity ctx EntityKind.Command [body] anonymous|> setEntity ctx node |> ignore
+      ctx
+
+/// Bind entities to all nodes in the program
+let bindProgram ctx (program:Program) =
+  ctx.Bound.Clear()
+  program.Body.Node |> List.fold bindCommand ctx |> ignore
+  ctx.Bound.ToArray()
+  
+/// Create a new binding context - this stores cached entities
+let createContext name =
+  let root = 
+    { Kind = EntityKind.Root; Antecedents = []; Errors = []
+      Name = { Name = name }; Symbol = createSymbol(); Type = None }
+  { Table = System.Collections.Generic.Dictionary<_, _>(); 
+    Bound = ResizeArray<_>(); Variables = Map.empty; 
+    Scope = root; Root = root }
---
+++ tests/thegamma-tests/binder.fs
@@ -0,0 +1,80 @@
+﻿#if INTERACTIVE
+#r "../../src/thegamma/bin/Debug/thegamma.dll"
+#r "../../packages/NUnit/lib/net45/nunit.framework.dll"
+#else
+[<NUnit.Framework.TestFixture>]
+module TheGamma.Tests.Binder
+#endif
+open TheGamma
+open TheGamma.Common
+open NUnit.Framework
+
+// --------------------------------------------------------------------------------------
+// Binder tests
+// --------------------------------------------------------------------------------------
+
+/// Type-safe assertion
+let equal (expected:'T) (actual:'T) = Assert.AreEqual(expected, actual)
+
+/// Check that nth commands of programs have equal/not equal entities
+let checkCommands eq n (p1:Program) (p2:Program) = 
+  equal eq (p1.Body.Node.[n].Entity.Value.Symbol = p2.Body.Node.[n].Entity.Value.Symbol)
+
+/// Parse sample that's indented with 2 spaces
+let parse (code:string) = 
+  let code = code.Replace("\n  ", "\n") 
+  code, code |> Parser.parseProgram |> fst
+
+/// Olympic sample code (in process of writing if `not completed`)
+let olympicSample complete = 
+  """
+  let data1 =
+    olympics.data
+      .'group data'.'by Athlete'.'sum Gold'.then
+      .'sort data'.'by Gold descending'
+      .then.paging.take(10)
+  """
+  + ( if complete then "let data2 = data1.'get the data'"
+      else "let data2 = data1.'get th" )
+  + """
+  table.create(data2).set(title="yadda")"""
+
+/// Disable all logging when running tests
+do Log.setEnabled(set [])
+
+[<Test>]
+let ``Binder reuses entities when binding twice`` () =
+  let ctx = Binder.createContext("script1")
+  let _, p1 = parse (olympicSample true)
+  let e1 = Binder.bindProgram ctx p1
+  let c1 = ctx.Table |> ListDictionary.count
+  let _, p2 = parse (olympicSample true)
+  let e2 = Binder.bindProgram ctx p2
+  let c2 = ctx.Table |> ListDictionary.count
+  equal c1 c2 
+
+[<Test>]
+let ``Binder reuses some entities when one member is changed`` () =
+  let ctx = Binder.createContext("script1")
+  let _, p1 = parse (olympicSample false)
+  let e1 = Binder.bindProgram ctx p1
+  let c1 = ctx.Table |> ListDictionary.count
+  let _, p2 = parse (olympicSample true)
+  let e2 = Binder.bindProgram ctx p2
+  let c2 = ctx.Table |> ListDictionary.count
+  equal true (c2 > c1 && c2 < c1*2)
+  checkCommands true 0 p1 p2
+  checkCommands false 1 p1 p2
+  checkCommands false 2 p1 p2
+
+[<Test>]
+let ``Binder binds all names in a sample program`` () =
+  let ctx = Binder.createContext("script1")
+  let code, p1 = parse (olympicSample true)
+  let e1 = Binder.bindProgram ctx p1
+  let bound = [ for rng, e in e1 -> code.Substring(rng.Start, rng.End - rng.Start + 1) ]
+  let names = 
+    [ "data1"; "olympics"; "data"; "'group data'"; "'by Athlete'"; "'sum Gold'"; 
+      "'sort data'"; "'by Gold descending'"; "paging"; "take"; "10"; "then"
+      "data2"; "'get the data'"; "table"; "create"; "set"; "title"; "\"yadda\"" ]
+  equal true (Set.isSubset (set names) (set bound))
