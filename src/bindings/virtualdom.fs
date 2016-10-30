b6d8426e6f16691e453c883cf9f437b7600b2098
Tomas Petricek <tomas@tomasp.net>
2016-10-30 21:37:58 +0000

    Inline Virtual DOM bindings, update paths

---
+++ src/bindings/virtualdom.fs
@@ -0,0 +1,17 @@
+﻿module Fable.Helpers.Virtualdom
+
+open Fable.Core
+open Fable.Core.JsInterop
+open System.Diagnostics
+
+[<Import("h","virtual-dom")>]
+let h(arg1: string, arg2: obj, arg3: obj[]): obj = failwith "JS only"
+
+[<Import("diff","virtual-dom")>]
+let diff (tree1:obj) (tree2:obj): obj = failwith "JS only"
+
+[<Import("patch","virtual-dom")>]
+let patch (node:obj) (patches:obj): Fable.Import.Browser.Node = failwith "JS only"
+
+[<Import("create","virtual-dom")>]
+let createElement (e:obj): Fable.Import.Browser.Node = failwith "JS only"
